8fa4d3f72e4d10539bb54bc92de3d7399dd28625
Jesse <jesse26603@live.ytech.edu>
2026-03-04 10:02:02 -0500

    Revert "Windowed and Full Screen"
    This reverts commit fe206e81bba6847f09671798fdbd917684710407.

--- src/PolyGone/Scenes/MenuScene.cs
+++ src/PolyGone/Scenes/MenuScene.cs
@@ -114,8 +114,7 @@ namespace PolyGone
             }
             else if (_selectedIndex == 1)
             {
-                // Options
-                _sceneManager.AddScene(new OptionsScene(_content, _sceneManager, _graphics));
+                // Options (placeholder for now)
             }
             else if (_selectedIndex == 2)
             {
