40cdd81239f38d417531fede6658f71e00fe213c
Jesse <jesse26603@live.ytech.edu>
2026-04-10 10:19:51 -0400

    Automapping Beginning
    Began working on the automapping rules for future use in level creation.

---
+++ TiledAssets/TiledSets/PolygoneMgSheet.tsx
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="PolygoneMgSheet" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../../../../../Downloads/PolygoneMgSheet.png" width="256" height="256"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="8" type="Semi">
+  <objectgroup draworder="index" id="5">
+   <object id="5" type="Semi" x="0" y="0" width="32" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="0" width="32" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Semi" x="0" y="0" width="32" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Se" x="0" y="0" width="32" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="3">
+   <object id="2" type="S" x="0" y="32">
+    <polygon points="0,0 32,-32 32,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
