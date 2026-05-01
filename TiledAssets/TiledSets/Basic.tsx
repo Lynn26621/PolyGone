ec5fd1b30cdc4e8834e7665633d8b75956fa656c
Jesse <jesse26603@live.ytech.edu>
2026-05-01 10:44:54 -0400

    Quick Tiled Set Fixes
    Tiled sets were broken due to shifting image names.

--- TiledAssets/TiledSets/Basic.tsx
+++ TiledAssets/TiledSets/Basic.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="PolyGoneTextureSheet" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="../../src/PolyGone/Content/Textures/Tiles/PolyGoneTextureSheet.png" width="64" height="64"/>
+<tileset version="1.10" tiledversion="1.12.1" name="Basic" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="../../src/PolyGone/Content/Textures/Tiles/PolyGoneBasic.png" width="64" height="64"/>
 </tileset>
